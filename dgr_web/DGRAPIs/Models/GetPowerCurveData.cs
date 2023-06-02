110a87a5c54e4c6184a0e257aab617ed68540485
Haresh-729 <kuradeharesh4002@gmail.com>
2023-06-02 19:21:35 +0530

    few changed left out files.

--- dgr_web/DGRAPIs/Models/GetPowerCurveData.cs
+++ dgr_web/DGRAPIs/Models/GetPowerCurveData.cs
@@ -5,12 +5,15 @@ using System.Threading.Tasks;
 
 namespace DGRAPIs.Models
 {
-    public class InsertWindPowerCurve
+    public class GetPowerCurveData
     {
         public int power_curve_id { get; set; }
         public string site { get; set; }
         public int site_id { get; set; }
         public double active_power { get; set; }
         public double wind_speed { get; set; }
+        public dynamic date { get; set; }
+        public double avg_active_power { get; set; }
+        public double avg_wind_speed { get; set; }
     }
 }
