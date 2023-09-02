59563854d06797fd5689a5733d59116103df9bb9
SoftelTechnology <info@softeltech.in>
2023-09-02 18:11:10 +0530

    main branch and mychanges merged

--- dgr_web/DGRAPIs/Models/WindViewDailyLoadShedding.cs
+++ dgr_web/DGRAPIs/Models/WindViewDailyLoadShedding.cs
@@ -9,9 +9,9 @@ namespace DGRAPIs.Models
     {
         public string Site { get; set; }
         public dynamic Date { get; set; }
-        public string Start_Time { get; set; }
-        public string End_Time { get; set; }
-        public string Total_Time { get; set; }
+        public dynamic Start_Time { get; set; }
+        public dynamic End_Time { get; set; }
+        public dynamic Total_Time { get; set; }
         public double Permissible_Load_MW { get; set; }
         public double Gen_loss_kWh { get; set; }
 
