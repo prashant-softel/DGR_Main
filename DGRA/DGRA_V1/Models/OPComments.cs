a62cb35c0bdb3ebc05abcbc746a2ed6973d6e0ed
Haresh-729 <kuradeharesh4002@gmail.com>
2024-02-01 01:33:59 +0530

    delete monthly comment frotend completed.

--- DGRA/DGRA_V1/Models/OPComments.cs
+++ DGRA/DGRA_V1/Models/OPComments.cs
@@ -7,7 +7,7 @@ namespace DGRA_V1.Models
 {
     public class OPComments
     {
-        //month, month_no, year, type, spv, site_id, BD_type, isDeleted, isMonthly, comment, added_by, updated_by
+        //opc_id, month, month_no, year, type, spv, site_id, site, bd_type, isDeleted, isMonthly, isSPV, comment, added_at, added_by, updated_by, updated_at, updated_comment
         public int opc_id { get; set; }
         public string month { get; set; }
         public int month_no { get; set; }
@@ -15,14 +15,20 @@ namespace DGRA_V1.Models
         public int type { get; set; }
         public string spv { get; set; }
         public int site_id { get; set; }
-        public int BDType { get; set; }
+        public string site { get; set; }
+        //public int BDType { get; set; }
+        public int bd_type { get; set; }
         public int isDeleted { get; set; }
         public int isMonthly { get; set; }
+        public int isSPV { get; set; }
         public string comment { get; set; }
-        public int added_by { get;set; }
-        public int added_at { get;set; }
-        public int updated_by { get;set; }
-        public int updated_at { get;set; }
+        public int added_by { get; set; }
+        public int added_at { get; set; }
+        public int updated_by { get; set; }
+        public int updated_at { get; set; }
+        public string updated_comment {get;set;}
+        public int deleted_by { get; set; }
+        public string deleted_at { get; set; }
 
     }
 }
--- dgr_web/DGRAPIs/Models/OPComments.cs
+++ dgr_web/DGRAPIs/Models/OPComments.cs
@@ -5,30 +5,9 @@ using System.Threading.Tasks;
 
 namespace DGRAPIs.Models
 {
-    //public class OPComments
-    //{
-    //    public int opc_id { get; set; }
-    //    public string month { get; set; }
-    //    public int month_no { get; set; }
-    //    public int year { get; set; }
-    //    public int type { get; set; }
-    //    public int site_id { get; set; }
-    //    public string site { get; set; }
-    //    public int spv_id { get; set; }
-    //    public string spv { get; set; }
-    //    public int BD_type { get; set; }
-    //    public int isDeleted { get; set; }
-    //    public int isMonthly { get; set; }
-    //    public string comment { get; set; }
-    //    public int added_by { get;set; }
-    //    public int added_at { get;set; }
-    //    public int updated_by { get;set; }
-    //    public int updated_at { get;set; }
-
-    //}
     public class OPComments
     {
-        //month, month_no, year, type, spv, site_id, BD_type, isDeleted, isMonthly, comment, added_by, updated_by
+        //opc_id, month, month_no, year, type, spv, site_id, site, bd_type, isDeleted, isMonthly, isSPV, comment, added_at, added_by, updated_by, updated_at, updated_comment
         public int opc_id { get; set; }
         public string month { get; set; }
         public int month_no { get; set; }
@@ -47,9 +26,33 @@ namespace DGRAPIs.Models
         public int added_at { get; set; }
         public int updated_by { get; set; }
         public int updated_at { get; set; }
+        public int deleted_by { get; set; }
+        public string deleted_at { get; set; }
 
     }
+    public class OPCFetch
+    {
+        //month, month_no, year, type, spv, site_id, BD_type, isDeleted, isMonthly, comment, added_by, updated_by
+        public string opc_id { get; set; }
+        public string month { get; set; }
+        public string month_no { get; set; }
+        public string year { get; set; }
+        public string type { get; set; }
+        public string spv { get; set; }
+        public string site_id { get; set; }
+        public string site { get; set; }
+        public string bd_type { get; set; }
+        public string isDeleted { get; set; }
+        public string isMonthly { get; set; }
+        public string isSPV { get; set; }
+        public string comment { get; set; }
+        public string updated_comment { get; set; }
+        public string added_by { get; set; }
+        public string added_at { get; set; }
+        public string updated_by { get; set; }
+        public string updated_at { get; set; }
 
+    }
     public class OPSPV
     {
         public int spv_id { get; set; }
